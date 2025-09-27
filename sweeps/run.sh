c067831f4f1797ce4365db76a1033b5e244297df
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2025-09-27 06:09:40 +0000

    updates  runsh

--- sweeps/run.sh
+++ sweeps/run.sh
@@ -1,4 +1,4 @@
 NGPU=${NGPU:-"1"}
 PYTORCH_ALLOC_CONF="expandable_segments:True" \
 
-uv run torchrun --local-ranks-filter 0 --nproc_per_node=${NGPU} src/zeroband/train.py "$@"
+uv run torchrun --local-ranks-filter 0 --nproc_per_node=${NGPU} --master_port=0 src/zeroband/train.py "$@"
